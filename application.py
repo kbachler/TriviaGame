125488b09c4eb942bf667ec3202ef2fb0392fadc
Kayla Bachler <bachlerkayla@gmail.com>
2019-03-16 00:08:36 -0700

    fixed user sessions id

--- application.py
+++ application.py
@@ -19,8 +19,8 @@ table = db.Table('triviadb')
 session_token = requests.get('https://opentdb.com/api_token.php?command=request')
 token = json.loads(session_token.content.decode('utf-8'))['token']
 
-uniqueIDs = []
-counter = 0     # Global counter for unique session IDs
+uniqueIDs = dict()  # Dictionary for sessionIDs and session values
+counter = 0         # Global counter for unique session IDs
 num_times = 0
 num_correct = 0 
 num_total = 10
@@ -35,149 +35,165 @@ category_list = {
 				'Animals':'https://opentdb.com/api.php?amount=10&category=27&type=multiple&token=' + token,
 				'Mythology':'https://opentdb.com/api.php?amount=10&category=20&type=multiple&token=' + token
 				}
-category_chosen = False
 question_num = 0
 
 @application.route('/')
 def initialize():
   return redirect('/home')
 
-@application.route('/home')
-def home():
+def check_session():
   id = request.cookies.get('session_id')    # Get the session_id, will be None if not existing
-  temp = False
 
   # If id is set and returns back a valid ID
-  if len(uniqueIDs) != 0:
-    for i in uniqueIDs:
-      print("i ==", i)
-      print("session id ==", id)
-      if id == i:
-        temp = True
-        print("Session id already saved")
-
-  # If we don't, create one:
-  if temp == False:
+  if "session_"+str(id) in uniqueIDs:
+    print("Session id already saved")
+    return {"foundKey":True, "response": "session_"+str(id)} # response holds unique session id
+  else:
+    # If we don't, create one:
     global counter
     counter += 1             # Increment global counter for unique session ID
     my_id = str(counter)
-    uniqueIDs.append(my_id)  # Add to the end of our unique ID array
+    uniqueIDs["session_"+my_id] = {}    # Add to the end of our unique ID array
     print(uniqueIDs)
-    resp = make_response(redirect('/home'))
+    resp = make_response(redirect('/home')) 
     resp.set_cookie(key='session_id', value=my_id)
-    return resp
+    return {"foundKey": False, "response": resp} 
 
-  return render_template('home.html', title='Home Page')
+@application.route('/home')
+def home():
+  session_status = check_session()         # Get the session id
+  if session_status["foundKey"]:
+    return render_template('home.html', title='Home Page')
+  else:
+    return session_status["response"]        # Sets cookie and redirects
 
 @application.route('/choose_category', methods=['POST'])
 def choose_category():
-	global category
-	category = request.form['choice']
-	return redirect('/start_game')
+  session_status = check_session()        # Get the session id
+  if session_status["foundKey"]:
+    category = request.form['choice']
+    uniqueIDs[session_status["response"]] = {"current_category": category, "num_times": 0, "num_correct": 0,"num_total": 10, "question_num": 0, "category_data": {}}
+    return redirect('/start_game')
+  else:
+    return session_status["response"]      # Sets cookie and redirects
 
 # Starts the Trivia game instance
 @application.route('/start_game')
 def start_game():
-	global category, category_chosen, response, question_num
+  session_status = check_session()        # Get the session id
+  if session_status["foundKey"]:
+    session_info = uniqueIDs[session_status["response"]]
+    if session_info["num_times"] >= session_info["num_total"] or session_info["question_num"] >= session_info["num_total"]:
+      return render_template('user_creation.html', title='Create User')
 
-	if num_times >= num_total or question_num >= num_total:
-		return render_template('user_creation.html', title='Create User')
+    if not session_info["category_data"]:
+      response = requests.get(category_list[session_info["current_category"]])
+      data = response.content.decode('utf-8')
+      session_info["category_data"] = json.loads(data)
+      print(session_info["category_data"])
 
-	if category_chosen == False:
-		response = requests.get(category_list[category])
-		category_chosen = True
+    # Resets token if questions are all exhausted
+    if session_info["category_data"]['response_code'] == 4:
+      global session_token, token
+      session_token = requests.get('https://opentdb.com/api_token.php?command=reset&token=' + str(token))
+      token = json.loads(session_token.content.decode('utf-8'))['token']
+      response = requests.get(category_list[session_info["category"]])
+      data = response.content.decode('utf-8')
+      session_info["category_data"] = json.loads(data)
 
-	data = response.content.decode('utf-8')
-	api_response = json.loads(data)
+    answers = []
 
-	# Resets token if questions are all exhausted
-	if api_response['response_code'] == 4:
-		global session_token, token
-		session_token = requests.get('https://opentdb.com/api_token.php?command=reset&token=' + str(token))
-		token = json.loads(session_token.content.decode('utf-8'))['token']
-		response = requests.get(category_list[category])
-		data = response.content.decode('utf-8')
-		api_response = json.loads(data)
-
-	answers = []
-
-	question = html.unescape(api_response['results'][question_num]['question'])
-	for i in api_response['results'][question_num]['incorrect_answers']:
-		answers.append(html.unescape(i))
-	correct_answer = html.unescape(api_response['results'][question_num]['correct_answer'])
-	answers.append(correct_answer)
-	shuffle(answers)
-	question_num += 1
-	return render_template('start_game.html', title='Game Start', question=question, \
-							ans1=answers[0], ans2=answers[1], ans3=answers[2], 		 \
-							ans4=answers[3], correct_answer=correct_answer,
-							bg_image='pingu.jpg', question_num=question_num)
+    question = html.unescape(session_info["category_data"]['results'][session_info["question_num"]]['question'])
+    for i in session_info["category_data"]['results'][session_info["question_num"]]['incorrect_answers']:
+      answers.append(html.unescape(i))
+    correct_answer = html.unescape(session_info["category_data"]['results'][session_info["question_num"]]['correct_answer'])
+    answers.append(correct_answer)
+    shuffle(answers)
+    session_info["question_num"] += 1
+    return render_template('start_game.html', title='Game Start', question=question, \
+                ans1=answers[0], ans2=answers[1], ans3=answers[2], 		 \
+                ans4=answers[3], correct_answer=correct_answer,
+                bg_image='pingu.jpg', question_num=session_info["question_num"])
+  else:
+    return session_status["response"]      # Sets cookie and redirects
 
 # Checks to see if user's answer is correct or not
 @application.route('/check_answer', methods=['POST'])
 def check_answer():
-	global num_times, num_correct
-	correct_answer = request.form['correct_answer']
-	# For testing purposes
-	print('YOUR INPUT: ' + request.form['ans'])
-	print('REAL ANS: ' + request.form['correct_answer'])
-	if request.form['ans'] == correct_answer:
-		num_correct += 1
-
-	num_times += 1
-	return redirect('/start_game')
+  session_status = check_session()        # Get the session id
+  if session_status["foundKey"]:
+    session_info = uniqueIDs[session_status["response"]]
+    correct_answer = request.form['correct_answer']
+    # For testing purposes
+    print('YOUR INPUT: ' + request.form['ans'])
+    print('REAL ANS: ' + request.form['correct_answer'])
+    if request.form['ans'] == correct_answer:
+      session_info["num_correct"] += 1
+    
+    session_info["num_times"] += 1
+    return redirect('/start_game')
+  else:
+    return session_status["response"]      # Sets cookie and redirects
 
 # Prints the user's game results
 @application.route('/results')
 def print_results():
-	global num_times, num_correct
-	return render_template('results.html', title='Results', \
-							num_correct=num_correct, num_times=num_times)
+  session_status = check_session()        # Get the session id
+  if session_status["foundKey"]:
+    session_info = uniqueIDs[session_status["response"]]  
+    return render_template('results.html', title='Results', \
+							num_correct=session_info["num_correct"], num_times=session_info["num_times"])
+  else:  
+    return session_status["response"]      # Sets cookie and redirects
 
 # Creates the user account to store info in various AWS services
 # and keep track of their high score
 @application.route('/user_creation', methods=['POST'])
 def create_user():
-	first_name = request.form['first_name']
-	last_name = request.form['last_name']
-	email = request.form['email']
+  session_status = check_session()        # Get the session id
+  if session_status["foundKey"]:
+    session_info = uniqueIDs[session_status["response"]]  
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
 
-	user_data = s3.Object('css490trivia', 'rawuserdata.txt')
-	list = user_data.get()['Body'].read().decode('utf-8').splitlines()
-	f = open('rawuserdata.txt', 'a')
-	for line in list:
-		f.write(line + '\n')
-	f.write(first_name + ',' + last_name + ',' + email + ',' + str(num_correct) + '\n')
+    user_data = s3.Object('css490trivia', 'rawuserdata.txt')
+    list = user_data.get()['Body'].read().decode('utf-8').splitlines()
+    f = open('rawuserdata.txt', 'a')
+    for line in list:
+      f.write(line + '\n')
+    f.write(first_name + ',' + last_name + ',' + email + ',' + str(session_info["num_correct"]) + '\n')
 
-	# Upload file to S3 bucket
-	#user_data.upload_file('rawuserdata.txt')
-	
-	# Upload entry into DB if score > prior high score
-	db_entry = {}
-	row = table.scan(FilterExpression=Attr('email').eq(email))
+    # Upload file to S3 bucket
+    #user_data.upload_file('rawuserdata.txt')
+    
+    # Upload entry into DB if score > prior high score
+    db_entry = {}
+    row = table.scan(FilterExpression=Attr('email').eq(email))
 
-	# If item does not exist in table
-	if row['Count'] == 0:
-		db_entry['email'] = email
-		db_entry['first_name'] = first_name
-		db_entry['last_name'] = last_name
-		db_entry['highscore'] = num_correct
-		table.put_item(Item=db_entry)
-	else:
-		prior_highscore = row['Items'][0]['highscore']
-		if num_correct > prior_highscore:
-			table.update_item(
-				Key={'email':email},
-				UpdateExpression='set highscore=:highscore',
-				ExpressionAttributeValues={':highscore': num_correct},
-				ReturnValues='UPDATED_NEW'
-			)
+    # If item does not exist in table
+    if row['Count'] == 0:
+      db_entry['email'] = email
+      db_entry['first_name'] = first_name
+      db_entry['last_name'] = last_name
+      db_entry['highscore'] = session_info["num_correct"]
+      table.put_item(Item=db_entry)
+    else:
+      prior_highscore = row['Items'][0]['highscore']
+      if session_info["num_correct"] > prior_highscore:
+        table.update_item(
+          Key={'email':email},
+          UpdateExpression='set highscore=:highscore',
+          ExpressionAttributeValues={':highscore': session_info["num_correct"]},
+          ReturnValues='UPDATED_NEW'
+        )
 
-	# Delete file locally
-	f.close()
-	#os.remove('rawuserdata.txt')
-
-	return redirect('/results')
+    # Delete file locally
+    f.close()
+    #os.remove('rawuserdata.txt')
+    return redirect('/results')
+  else:  
+    return session_status["response"]      # Sets cookie and redirects
 
 @application.route('/check_score', methods=['POST'])
 def check_score():
@@ -240,11 +256,6 @@ def display_leaderboard():
 # Restarts the game instance
 @application.route('/reset', methods=['POST'])
 def reset():
-	global num_times, num_correct, category_chosen, question_num
-	num_times = 0
-	num_correct = 0
-	category_chosen = False
-	question_num = 0
 	return redirect('/home')
 
 if __name__ == "__main__":
